8f569154497531db9581a3c80e05846c4ccd725f
Shawn Duong <me@shawnd.xyz>
2025-01-28 01:46:23 -0800

    2025 archive.

---
+++ static/bg/71a983c3dac9500a8a94a3f60d56302d.js
@@ -0,0 +1,107 @@
+async function sha1Hex(input) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(input);
+  const hashBuffer = await crypto.subtle.digest('SHA-1', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return hashArray.map(b => b.toString(16).padStart(2, '0')).join('').substring(0, 32);
+}
+async function instantiate(module, imports = {}) {
+  const adaptedImports = {
+    env: Object.assign(Object.create(globalThis), imports.env || {}, {
+      abort(message, fileName, lineNumber, columnNumber) {
+        // ~lib/builtins/abort(~lib/string/String | null?, ~lib/string/String | null?, u32?, u32?) => void
+        message = __liftString(message >>> 0);
+        fileName = __liftString(fileName >>> 0);
+        lineNumber = lineNumber >>> 0;
+        columnNumber = columnNumber >>> 0;
+        (() => {
+          // @external.js
+          throw Error(`${message} in ${fileName}:${lineNumber}:${columnNumber}`);
+        })();
+      },
+      "console.log"(text) {
+        // ~lib/bindings/dom/console.log(~lib/string/String) => void
+        text = __liftString(text >>> 0);
+      },
+    }),
+  };
+  const { exports } = await WebAssembly.instantiate(module, adaptedImports);
+  const memory = exports.memory || imports.env.memory;
+  const adaptedExports = Object.setPrototypeOf({
+    reality() {
+      // assembly/index/reality() => ~lib/string/String
+      return __liftString(exports.reality() >>> 0);
+    },
+    recognition(jsSecretData) {
+      // assembly/index/recognition(~lib/typedarray/Uint8Array) => void
+      jsSecretData = __lowerTypedArray(Uint8Array, 4, 0, jsSecretData) || __notnull();
+      exports.recognition(jsSecretData);
+    },
+    consciousness(catIdx) {
+      // assembly/index/consciousness(u32) => ~lib/string/String
+      const result = __liftString(exports.consciousness(catIdx) >>> 0);
+      if (result.length === 0) {
+        return undefined;
+      } else {
+        return JSON.parse(result);
+      }
+    },
+    awareness(str) {
+      // assembly/index/awareness(~lib/string/String) => bool
+      str = __lowerString(str) || __notnull();
+      return exports.awareness(str) != 0;
+    },
+  }, exports);
+  function __liftString(pointer) {
+    if (!pointer) return null;
+    const
+      end = pointer + new Uint32Array(memory.buffer)[pointer - 4 >>> 2] >>> 1,
+      memoryU16 = new Uint16Array(memory.buffer);
+    let
+      start = pointer >>> 1,
+      string = "";
+    while (end - start > 1024) string += String.fromCharCode(...memoryU16.subarray(start, start += 1024));
+    return string + String.fromCharCode(...memoryU16.subarray(start, end));
+  }
+  function __lowerString(value) {
+    if (value == null) return 0;
+    const
+      length = value.length,
+      pointer = exports.__new(length << 1, 2) >>> 0,
+      memoryU16 = new Uint16Array(memory.buffer);
+    for (let i = 0; i < length; ++i) memoryU16[(pointer >>> 1) + i] = value.charCodeAt(i);
+    return pointer;
+  }
+  function __lowerTypedArray(constructor, id, align, values) {
+    if (values == null) return 0;
+    const
+      length = values.length,
+      buffer = exports.__pin(exports.__new(length << align, 1)) >>> 0,
+      header = exports.__new(12, id) >>> 0;
+    __setU32(header + 0, buffer);
+    __dataview.setUint32(header + 4, buffer, true);
+    __dataview.setUint32(header + 8, length << align, true);
+    new constructor(memory.buffer, buffer, length).set(values);
+    exports.__unpin(buffer);
+    return header;
+  }
+  function __notnull() {
+    throw TypeError("value must not be null");
+  }
+  let __dataview = new DataView(memory.buffer);
+  function __setU32(pointer, value) {
+    try {
+      __dataview.setUint32(pointer, value, true);
+    } catch {
+      __dataview = new DataView(memory.buffer);
+      __dataview.setUint32(pointer, value, true);
+    }
+  }
+  return adaptedExports;
+}
+async function loadWasm() {
+  window['mind'] = await instantiate(
+    await WebAssembly.compileStreaming(fetch(`/static/bg/${await sha1Hex('mind')}.wasm`)), {}
+  );
+  return window['mind'];
+}
